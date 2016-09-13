731c9f9bc0851f656b9d05813955b761744dbba2
John Kimionis <ikimionis@gatech.edu>
2016-09-13 12:40:21 -0400

    update HTTP endpoints

--- ExpressExample/index.js
+++ ExpressExample/index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/hareen', function (req, res) {
+  res.send('Hello Hareen!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
