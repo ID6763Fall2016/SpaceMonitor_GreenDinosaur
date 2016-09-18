5c42fe9ad744ee34f19f739438185bcd6497f32f
John Kimionis <ikimionis@gatech.edu>
2016-09-18 16:55:25 -0400

    added door counter

--- Space_Monitor/app.js
+++ Space_Monitor/app.js
@@ -15,13 +15,13 @@ server.get('/', function (req, res) {
 });
 
 
-/******* Button, LED ******/
-// button is attaced to pin 17, led to 18
+/******* GPIO ******/
+// button is attaced to pin 17, LED to 18
 var GPIO = require('onoff').Gpio;
-var led = new GPIO(18, 'out');
-// var button = new GPIO(17, 'in', 'both');
+var LED_connection_status = new GPIO(18, 'out');
+var button = new GPIO(17, 'in', 'both');
 
-
+/******* Check Internet Connection Status *******/
 var previous_online_status = false;
 
 setInterval(function(){
@@ -30,8 +30,34 @@ setInterval(function(){
     // only make changes to LED when something changes in connectivity
     if (online != previous_online_status) {
       console.log(online);
-      led.writeSync(online ? 0 : 1);
+      LED_connection_status.writeSync(online ? 0 : 1);
     }
     previous_online_status = online;
   });
 },60000);
+
+/******* Measure Door openings/minute *******/
+door_counter = 0;
+door_interval = 1;    // minutes
+
+function count_door_openings(){
+  door_counter++;
+  console.console.log("door: " + door_counter);
+}
+
+// reset door counter every door_interval minutes
+setInterval(function(){
+  door_counter = 0;
+},door_interval*60);
+
+// watch for Door opening actions and call callback function
+button.watch(count_door_openings);
+
+
+
+
+
+/******* Assess status of Cafe *******/
+function cafe_status() {
+
+}
