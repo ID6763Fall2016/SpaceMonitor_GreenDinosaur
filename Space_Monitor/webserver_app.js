68949ed57df6fa74254d55e4d0b690d77c18b25f
John Kimionis <ikimionis@gatech.edu>
2016-09-21 15:57:47 -0400

    design live view

--- Space_Monitor/webserver_app.js
+++ Space_Monitor/webserver_app.js
@@ -3,16 +3,17 @@ var app = express();
 
 // serve static pages in public_html folder
 app.use(express.static('public_html'));
+app.use(express.static('sm'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-//hi
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+    //hi
 });
 
-app.get('/hareen', function (req, res) {
-  res.send('Hello Hareen!');
+app.get('/hareen', function(req, res) {
+    res.send('Hello Hareen!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
